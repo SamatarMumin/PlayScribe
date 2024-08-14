9e9523c13b6ec2abd058de8057e7d096810db66c
SamatarMumin <35471795+SamatarMumin@users.noreply.github.com>
2024-08-14 19:27:16 -0400

    styling changes and adding create page form

--- src/pages/games/create.tsx
+++ src/pages/games/create.tsx
@@ -2,10 +2,27 @@ import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
-export default function gamesPage() {
 
+
+const CreateGamePost = () => {
+  const {user} = useUser();
+  if (!user) return null;
+  
+  return (<div className="flex">
+    <img src={user.profileImageUrl} alt="Profile Image" className="object-left-top h-16 w-16 rounded-full"></img>
+  </div>);
+  
+}
+
+export default function gamesPage() {
     return ( <div>
-      <h1>testubg</h1>
+      <CreateGamePost></CreateGamePost>
+      <form className="flex border-spacing-8">
+        <input type="text" name="Game Title"></input>
+        <input></input>
+        <button type="submit"> Submit</button>
+      </form>
+      
     </div>);
   }
   
